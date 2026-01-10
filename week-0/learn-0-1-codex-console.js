e1cade419aed02d13a44cffea3e4dcd2f3c7356c
Brigid Skojec <bskojec@ewu.edu>
2026-01-10 05:12:27 +0000

    Add learn 0-1 Codedex exercises

---
+++ week-0/learn-0-1-codex-console.js
@@ -0,0 +1,87 @@
+// ============================================
+// TUTORIAL 0-1: CODEDEX CONSOLE FUNDAMENTALS
+// Student: Brigid
+// Date: 1/9/2026
+// ============================================
+
+// --------------------------------------------
+// EXERCISE 1: SETTING UP
+// --------------------------------------------
+console.log("Hello internet!");
+// Add your custom greeting below:
+console.log("I can't wait to understand JavaScript!");
+
+// --------------------------------------------
+// EXERCISE 2: THE CONSOLE
+// Your favorite food
+// --------------------------------------------
+console.log("Pasta");
+
+// --------------------------------------------
+// EXERCISE 3: LETTER TREE
+// The pattern: a, b c, d e f, g h i j, k
+// --------------------------------------------
+console.log("   a   ");
+console.log("  b c ");
+console.log(" d e f ");
+console.log("g h i j");
+console.log("   k  ");
+
+// --------------------------------------------
+// EXERCISE 4: SECRET RECIPE
+// Use comments to document your recipe
+// Include a "secret ingredient" comment
+// --------------------------------------------
+// I just chose a random blondie recipe because I'm craving them right now
+// We should just have everyone bring in the food from their recipes
+// Bridge Sko
+
+console.log("Ingredients:");
+console.log("1¾ cups all-purpose flour");
+console.log("1 teaspoon baking powder");
+console.log("¼ teaspoon baking soda");
+console.log("¾ teaspoon salt");
+console.log("1 cup packed light brown sugar");
+console.log("½ cup unsalted butter melted");
+console.log("1 large egg room temperature");
+console.log("2 teaspoons vanilla extract");
+
+/* 
+As a little special something else
+you could add any mix in, such as white, semi-sweet chocolate 
+or butterscotch chips; chopped nuts such as walnuts, pecans, or almonds...
+*/
+
+console.log('=================================')
+console.log("Instructions:");
+console.log("Preheat the oven to 350°F. Line a 8×8-inch baking pan with parchment paper or foil, leaving a 2-inch overhang on at least 2 of the sides.");
+console.log("In a medium bowl, whisk together the flour, baking powder, baking soda, and salt.");
+console.log("In a large mixing bowl, whisk to combine the brown sugar and melted butter. Add the egg, and vanilla and whisk vigorously until the mixture is thick and well combined");
+console.log("Add the dry mixture to the wet mixture and stir together with a spatula until almost combined. Add ¾ cup of mix-in’s and stir until they are well distributed.");
+console.log("Transfer the dough to the prepared baking pan and spread into an even layer. Press the remaining ¼ cup of mix in’s into the top.");
+console.log("Bake for 32 to 36 minutes or until the edges are set and golden brown and a toothpick inserted into the center comes out clean. Let the cookie bars cool completely in the pan before cutting.");
+
+/*
+Little secret:
+Using too much flour is a surefire way to end up with dry, crumbly blondies.
+ fluff the flour in its bag or container and scoop it into your measuring cup. Level off the top with a knife.
+ Also, A metal pan will bake up a better blondie with crispy edges and a chewy center.
+ */
+
+
+// --------------------------------------------
+// EXERCISE 5: RECEIPT
+// Format a receipt from a recent purchase
+// --------------------------------------------
+console.log("===================================");
+console.log("              Walmart ✢           ");
+console.log("-----------------------------------");
+console.log("             Order Details         ");
+console.log("-----------------------------------");
+console.log(" Item         Quantity     Price   ");
+console.log("-----------------------------------");
+console.log(" SMSG T7 1TB      1        99.97"   );
+console.log("         TAX      9.200%    9.20"   );
+console.log("-----------------------------------");
+console.log("TOTAL:                    109.17"   );
+console.log("===================================");
