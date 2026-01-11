e3fefee62201154b5b58e4ac96eaddf067ead9d3
Brigid Skojec <bskojec@ewu.edu>
2026-01-11 03:39:32 +0000

    continued tutorial for behavioral layer assignment

--- week-0/behvioral-layer/scripts/main.js
+++ week-0/behvioral-layer/scripts/main.js
@@ -1,4 +1,10 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/fox1.png") {
+    myImage.setAttribute("src", "images/fox2.png");
+  } else {
+    myImage.setAttribute("src", "images/fox1.png");
+  }
+});
