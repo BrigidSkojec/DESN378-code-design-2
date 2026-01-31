9e4173c23dfcf471057263ba3426ae67434bd917
Brigid Skojec <bskojec@ewu.edu>
2026-01-31 03:10:28 +0000

    Add theme persistence by saving and loading user-selected theme

--- week-3/portfolio/scripts/main.js
+++ week-3/portfolio/scripts/main.js
@@ -1,3 +1,10 @@
+// Load saved theme (if any)
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+  document.documentElement.dataset.theme = savedTheme;
+}
+
 const toggle = document.querySelector(".theme-toggle");
 document.documentElement.setAttribute("data-theme", "light");
 
@@ -5,4 +12,5 @@ toggle.addEventListener("click", function () {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   const newTheme = currentTheme === "light" ? "dark" : "light";
   document.documentElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem('theme', newTheme); // Save to memory 
 });
